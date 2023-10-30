b6c40071b89d96cfaf61c92c02b179d15e0cf92c
Dmitry Petrov <Dima.demon.net@mail.ru>
2023-10-30 22:24:13 +0400

    bugfix: Исправить файл config

--- vite.config.js
+++ vite.config.js
@@ -5,17 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    assetsDir: '', // Отключаем папку assets
-
     rollupOptions: {
       output: {
         assetFileNames: (assetInfo) => {
           if (/\.(woff|woff2|eot|ttf|otf)$/i.test(assetInfo.name)) {
             return `fonts/${assetInfo.name}`;
-          } else if (/\.(png|svg|jpg|jpeg|gif)$/i.test(assetInfo.name)) {
-            return `images/${assetInfo.name}`;
           }
-          return assetInfo.name; // Оставляем остальные файлы в корне
+          return `assets/${assetInfo.name}`; // Оставляем остальные файлы в папке assets
         },
       },
     },
